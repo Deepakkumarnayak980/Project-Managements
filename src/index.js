77cdfef58b7e80c8340fe0f20cfe14013d7e20c5
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2026-03-14 08:38:39 +0530

    destructer the files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import dotenv from "dotenv";
-import express from 'express';
+import { app } from "./app.js";
 
 dotenv.config();
-const app=express()
-const PORT =process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-app.get('/',(req,res)=>{
-    res.send("hello Deepak")
-})
-
-app.listen(PORT,()=>{
-    console.log(`app listening the Port:${PORT}`);
-    
-})
+app.listen(PORT, () => {
+  console.log(`app listening the Port:${PORT}`);
+});
