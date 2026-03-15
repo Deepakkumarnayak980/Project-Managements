e54599f2ef08d993c3bcef440789bf96c857502e
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2026-03-15 09:06:16 +0530

    hash tha password and handle mongooes methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema(
   {
@@ -71,4 +72,14 @@ const userSchema = new Schema(
   }
 );
 
-export const User = mongoose.model("User", userSchema);
+userSchema.pre("save",async function (next) {
+  if(!this.isModified("password")) return next() ;
+  this.password =await  bcrypt.hash(this.password,10)
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+export const User = mongoose.model("User", userSchema); 
