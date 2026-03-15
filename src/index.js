e987d7f2acb7dc67dbce75c1f79ea36a3233834c
Deepak Kumar nayak <deepakkumarnayak715@gmail.com>
2026-03-15 07:03:06 +0530

    mognoDB connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import { connectDB } from "./db/index.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`app listening the Port:${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`app listening the Port:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`MongoDB connection error `, err);
+    process.exit(1);
+  });
